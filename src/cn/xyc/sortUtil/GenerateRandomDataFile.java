ba112b4206ba29ae1b6d3c350847d1d300c074b1
andy0605 <andy413280603@163.com>
2017-06-01 23:06:54 +0800

    基础类

---
+++ src/cn/xyc/sortUtil/GenerateRandomDataFile.java
@@ -0,0 +1,27 @@
+package cn.xyc.sortUtil;
+
+import java.io.File;
+import java.io.FileWriter;
+
+/**
+ * 
+ **************************************************************************************************
+ * 生成随机整数文件的工具类
+ **************************************************************************************************
+ * @author andy<br/>
+ * 
+ *         代码时间:2016年9月17日 下午11:42:14
+ */
+public class GenerateRandomDataFile {
+
+	/**
+	 * 生成整数
+	 * 
+	 * @param fileName
+	 * @param num
+	 */
+	public static void generate(String fileName, int num) {
+
+	}
+
+}
